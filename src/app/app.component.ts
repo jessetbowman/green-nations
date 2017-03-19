4c9ea3e070617cf90014aa430730ed4d4196a487
Ketan Shukla <ketankshukla@gmail.com>
2017-03-18 20:49:33 -0700

    full commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { FrameworkConfigService, FrameworkConfigSettings } from '../fw/services/framework-config.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private FrameworkConfigService: FrameworkConfigService) {
+
+    let config: FrameworkConfigSettings = {
+      socialIcons: [
+        { imageFile: 'assets/social-fb-bw.png', alt: 'Facebook', link: 'http://facebook.com' },
+        { imageFile: 'assets/social-google-bw.png', alt: 'Google', link: 'http://google.com' },
+        { imageFile: 'assets/social-twitter-bw.png', alt: 'Twitter', link: 'http://twitter.com' }
+      ],
+      showLanguageSelector: true,
+      showUserControls: true,
+      showStatusBar: true,
+      showStatusBarBreakpoint: 800
+      
+    };
+
+    FrameworkConfigService.configure(config);
+  }
 }
